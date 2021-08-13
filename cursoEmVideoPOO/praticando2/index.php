3f0a6aa4282a35d031e26397eaf666599e63030e
diegoperesmarques <diegoperesmarques@gmail.com>
2021-08-13 19:02:48 -0300

    Curso em Vídeo POO - Praticando finalizado

--- cursoEmVideoPOO/praticando2/index.php
+++ cursoEmVideoPOO/praticando2/index.php
@@ -7,8 +7,20 @@
     <title>Sistema Farmacia</title>
 </head>
 <body>
+<pre>
 <?php
+require_once 'Vendedor.php';
+require_once 'Produto.php';
+require_once 'Estoquista.php';
+
+$vend = new Vendedor();
+$vend->setNome("Maria");
+$vend->setSalario(1000);
+$vend->setTurno("Manhã");
+print_r($vend);
+$vend->vender();
 
 ?>
+</pre>
 </body>
 </html>
