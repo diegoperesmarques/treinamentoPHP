72d7bd49e09e155a255ddb09902833caaadf512e
diegoperesmarques <diegoperesmarques@gmail.com>
2021-08-13 18:57:11 -0300

    Curso em Vídeo - Praticando - GET e SET do Produto

--- cursoEmVideoPOO/praticando2/Produto.php
+++ cursoEmVideoPOO/praticando2/Produto.php
@@ -4,5 +4,29 @@ class Produto {
     private $tipo;
     private $quantidade;
 
+    function getNome() {
+        return $this->nome;
+    }
+
+    function setNome($n) {
+        $this->nome = $n;
+    }
+
+    function getTipo() {
+        return $this->tipo;
+    }
+
+    function setTipo($t) {
+        $this->tipo = $t;
+    }
+
+    function getQuantidade() {
+        return $this->quantidade;
+    }
+
+    function setQuantidade($q) {
+        $this->quantidade = $q;
+    }
+
 }
 ?>
