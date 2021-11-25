9d86387212141ac5784bfc3288afd7bc0959e2df
diegoperesmarques <diegoperesmarques@hotmail.com>
2021-11-25 16:16:57 -0300

    DankiCode - Desenvolvimento Web Completo - Modulo 6 PHP Intermediário - Aula 03 - Sleep e Die

---
+++ dankiCode/modulo6/aula03/index.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+
+Durma o código por x segundos.
+sleep(3);
+
+echo 'Olá';
+
+sleep(3);
+echo 'dois';
+*/
+
+$nome = 'Diego';
+
+if ($nome == 'joao') {
+    echo 'continue';
+} else {
+    die("O script parou de ser executado");
+}
+
+sleep(3);
+
+echo 'Olá';
+
+sleep(3);
+echo 'dois';
+
+
+?>
