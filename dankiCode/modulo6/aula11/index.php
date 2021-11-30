67bd9423e563fa9459493365bc54457183001703
diegoperesmarques <diegoperesmarques@hotmail.com>
2021-11-30 10:52:19 -0300

    DankiCode - Desenvolvimento Web Completo - Modulo 6 PHP Intermediário - Aula 11 - Syntaxe alternativa no PHP

---
+++ dankiCode/modulo6/aula11/index.php
@@ -0,0 +1,25 @@
+<?php
+$nome = "guilherme";
+
+if ($nome == 'guilherme'):
+    echo 'Verdade';
+endif;
+
+echo '<br>';
+
+$contador = 0;
+while ($contador < 10):
+    $contador++;
+    echo 'oi';
+    echo '<br>';
+endwhile;
+
+$array = array();
+foreach($array as $key => $value):
+
+endforeach;
+
+for ($i = 0; $i < 10; $i++):
+    //Aqui vai o código
+endfor;
+?>
